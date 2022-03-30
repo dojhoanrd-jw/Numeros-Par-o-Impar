20d3fe53d2b1c4d769423e5fd0e52ab1c21c3ed4
Jhoan J. Ramírez <102481805+Dojhoanrd@users.noreply.github.com>
2022-03-30 20:44:24 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int x;
+
+printf("Escribe el número sin decimales: ");
+
+scanf("%d",&x);
+
+if (x%2==0)
+
+printf("El número es par");
+
+else
+
+printf("El número es impar");
+}
